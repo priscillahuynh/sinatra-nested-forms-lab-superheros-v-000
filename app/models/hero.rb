b40f48524ad662990a247b64f795b7b38f80f012
Priscilla H <priscillamhuynh@gmail.com>
2019-02-17 10:33:34 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,9 +1,14 @@
 class Hero 
   attr_reader :name, :power, :biography
-  
+  @@all =[] 
   def initialize(details)
     @name = details[:name]
     @power = details[:power]
     @biography = details[:biography]
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
   end
 end
